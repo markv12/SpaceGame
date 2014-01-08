4a8d677efaf944de3213dffac654523f153e62f4
Markv12 <swsteffes@gmail.com>
2014-01-08 23:18:56 +0900

    Added Start Menu
    Menu shows as the rest of the assets are loading for the main level

---
+++ SpaceThing/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+	private GUISkin skin;
+
+	const int buttonWidth = 100;
+	const int buttonHeight = 60;
+
+	
+	void Start()
+	{
+		// Load a skin for the buttons
+		skin = Resources.Load("StartButton") as GUISkin;
+	}
+	
+	void OnGUI()
+	{
+		GUI.skin = skin;
+		if (Application.CanStreamedLevelBeLoaded("GravityGame")){
+
+			// Draw a button to start the game
+			if (
+					GUI.Button(
+						new Rect(
+						Screen.width / 2 - (buttonWidth / 2),
+						(Screen.height*0.75f) - (buttonHeight / 2),
+						buttonWidth,
+						buttonHeight
+						),
+						"Start!"
+					)
+				)
+			{
+				// On Click, load the first level.
+				Application.LoadLevel("GravityGame");
+			}
+		}
+		else{
+			GUI.Label(
+					new Rect(
+					Screen.width / 2 - (buttonWidth / 2),
+					(Screen.height*0.75f) - (buttonHeight / 2),
+					buttonWidth,
+					buttonHeight
+					),
+					"Loading..."
+			);
+		}
+	}
+}
