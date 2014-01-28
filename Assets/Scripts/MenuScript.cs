6a8de36610e0d9bf878f5bf601990e39ff7334a1
Markv12 <swsteffes@gmail.com>
2014-01-28 14:56:21 +0900

    Changed start screen to use keyboard input

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -11,8 +11,9 @@ public class MenuScript : MonoBehaviour
 	const int buttonWidth = 100;
 	const int buttonHeight = 60;
 
-	public Texture2D progressBar;
-	public Texture2D progressBarCover;
+	public GameObject progressBar;
+	public GameObject progressBarCover;
+	public GameObject startMessage;
 
 	private float buttonPosX;
 	private float buttonPosY;
@@ -25,9 +26,36 @@ public class MenuScript : MonoBehaviour
 		skin = Resources.Load("StartButton") as GUISkin;
 		DontDestroyOnLoad(GameState.Instance);
 		GameState.Instance.startState();
+
+
+		float scaleFactor = 0.3f;
+		float pWidth = progressBar.transform.localScale.x;
+		float pHeight = progressBar.transform.localScale.y;
+		float coverWidth = progressBarCover.transform.localScale.x;
+		float coverHeight = progressBarCover.transform.localScale.y;
+		float xPosition = Screen.width / 2 - (pWidth / 2);
+		float yPosition = (Screen.height * 0.75f) - (pHeight / 2);
+	}
+
+	void Update(){
+		if(Application.CanStreamedLevelBeLoaded(1)){
+			if(progressBar.transform.localScale.x !=0){
+				progressBar.transform.localScale = new Vector3(0,0,0);
+				startMessage.transform.localScale = new Vector3(0.7f,0.7f,1f);
+			}
+		}
+		else{
+			Vector3 currentScale = progressBarCover.transform.localScale;
+			progressBarCover.transform.localScale = new Vector3(1f-Application.GetStreamProgressForLevel (1), currentScale.y, currentScale.z);
+		}
+
+		if(Input.GetKeyDown ("x")){
+			GameState.Instance.LoadLevel(1);
+		}
+		
 	}
 	
-	void OnGUI(){
+	/*void OnGUI(){
 		GUI.skin = skin;
 		if (Application.CanStreamedLevelBeLoaded(1)){
 
@@ -48,6 +76,7 @@ public class MenuScript : MonoBehaviour
 			}
 		}
 		else{
+
 			float scaleFactor = 0.3f;
 			float progress = Application.GetStreamProgressForLevel(1);
 			float pWidth = progressBar.width*scaleFactor;
@@ -68,6 +97,7 @@ public class MenuScript : MonoBehaviour
 			                         coverWidth - (coverWidth*progress),
 			                         coverHeight),
 			                progressBarCover);
+
 			GUI.Label(
 				new Rect(
 				Screen.width / 2 - (buttonWidth / 2),
@@ -78,5 +108,5 @@ public class MenuScript : MonoBehaviour
 				"Loading..."
 			);
 		}
-	}
+	}*/
 }
